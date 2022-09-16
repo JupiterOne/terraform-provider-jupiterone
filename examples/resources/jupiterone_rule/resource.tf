23756f582be0c583a107365258702a8959370191
Matthew Zember <matthew.zember@jupiterone.com>
2022-09-16 10:30:07 -0400

    update example

--- examples/resources/jupiterone_rule/resource.tf
+++ examples/resources/jupiterone_rule/resource.tf
@@ -40,3 +40,41 @@ resource "jupiterone_rule" "unencrypted_critical_data_stores" {
     ]
   EOF
 }
+
+
+resource "jupiterone_rule" "users_without_mfa" {
+  name = "users-without-mfa"
+  description = "Users who do not have mfa enabled."
+  polling_interval = "ONE_DAY"
+
+  question_id = jupiterone_question.users_without_mfa.id
+  
+  tags = ["critical"]
+
+  outputs = [
+    "queries.query0.total",
+    "alertLevel"
+  ]
+
+  operations = <<EOF
+    [
+      {
+        "when": {
+          "type": "FILTER",
+          "specVersion": 1,
+          "condition": "{{queries.query0.total != 0}}"
+        },
+        "actions": [
+          {
+            "targetValue": "HIGH",
+            "type": "SET_PROPERTY",
+            "targetProperty": "alertLevel"
+          },
+          {
+            "type": "CREATE_ALERT"
+          }
+        ]
+      }
+    ]
+  EOF
+}
