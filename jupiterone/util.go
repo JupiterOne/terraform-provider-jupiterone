0d6576ddd9b6288eb649129c561120f2913b8284
Matthew Zember <matthew.zember@gmail.com>
2022-12-07 08:22:18 -0500

    do not panic during diff suppresion

--- jupiterone/util.go
+++ jupiterone/util.go
@@ -24,14 +24,17 @@ func interfaceSliceToStringSlice(l []interface{}) []string {
 
 func jsonDiffSuppressFunc(k, oldValue, newValue string, d *schema.ResourceData) bool {
 	var old, new interface{}
+	// Errors during json.Unmarshal likely mean that newValue is empty or deleted.
+	// In this case we shouldn't suppress the diff and let downstream code
+	// handle the changes and updates.
 	err := json.Unmarshal([]byte(oldValue), &old)
 	if err != nil {
-		panic(err)
+		return false
 	}
 
 	err = json.Unmarshal([]byte(newValue), &new)
 	if err != nil {
-		panic(err)
+		return false
 	}
 
 	return reflect.DeepEqual(old, new)
