635462d49164c1e677270a068c67e00958b7c7be
Matthew Zember <matthew.zember@gmail.com>
2022-12-07 12:34:18 -0500

    update test suite

--- jupiterone/resource_rule_test.go
+++ jupiterone/resource_rule_test.go
@@ -19,7 +19,7 @@ func TestRuleInstance_Basic(t *testing.T) {
 	accProviders, cleanup := testAccProviders(t)
 	defer cleanup(t)
 	accProvider := testAccProvider(t, accProviders)
-	rName := acctest.RandomWithPrefix("tf-acc-test")
+	ruleName := "tf-provider-rule"
 	resourceName := "jupiterone_rule.test"
 
 	operations := getValidOperations()
@@ -30,15 +30,15 @@ func TestRuleInstance_Basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    accProviders,
-		CheckDestroy: testAccCheckRuleInstanceDestroy(ctx, accProvider),
+		CheckDestroy: testAccCheckRuleInstanceDestroy(ctx, resourceName, accProvider),
 		Steps: []resource.TestStep{
 			{
-				Config: testRuleInstanceBasicConfigWithOperations(rName, operations),
+				Config: testRuleInstanceBasicConfigWithOperations(ruleName, operations),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckRuleExists(ctx, accProvider),
+					testAccCheckRuleExists(ctx, resourceName, accProvider),
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					resource.TestCheckResourceAttr(resourceName, "version", "1"),
-					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "name", ruleName),
 					resource.TestCheckResourceAttr(resourceName, "description", "Test"),
 					resource.TestCheckResourceAttr(resourceName, "spec_version", "1"),
 					resource.TestCheckResourceAttr(resourceName, "polling_interval", "ONE_WEEK"),
@@ -57,12 +57,12 @@ func TestRuleInstance_Basic(t *testing.T) {
 				),
 			},
 			{
-				Config: testRuleInstanceBasicConfigWithOperations(rName, operationsUpdate),
+				Config: testRuleInstanceBasicConfigWithOperations(ruleName, operationsUpdate),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckRuleExists(ctx, accProvider),
+					testAccCheckRuleExists(ctx, resourceName, accProvider),
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					resource.TestCheckResourceAttr(resourceName, "version", "2"),
-					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "name", ruleName),
 					resource.TestCheckResourceAttr(resourceName, "description", "Test"),
 					resource.TestCheckResourceAttr(resourceName, "spec_version", "1"),
 					resource.TestCheckResourceAttr(resourceName, "polling_interval", "ONE_WEEK"),
@@ -90,11 +90,12 @@ func TestRuleInstance_Config_Errors(t *testing.T) {
 	accProvider := testAccProvider(t, accProviders)
 	rName := acctest.RandomWithPrefix("tf-acc-test")
 	ctx := context.Background()
+	resourceName := "jupiterone_rule.test"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    accProviders,
-		CheckDestroy: testAccCheckRuleInstanceDestroy(ctx, accProvider),
+		CheckDestroy: testAccCheckRuleInstanceDestroy(ctx, resourceName, accProvider),
 		Steps: []resource.TestStep{
 			{
 				Config:      testRuleInstanceBasicConfigWithOperations(rName, "not json"),
@@ -120,75 +121,72 @@ func getValidOperationsWithoutConditions() string {
 	return `[{"actions":[{"targetValue":"HIGH","type":"SET_PROPERTY","targetProperty":"alertLevel"},{"type":"CREATE_ALERT"}]}]`
 }
 
-func testAccCheckRuleExists(ctx context.Context, accProvider *schema.Provider) resource.TestCheckFunc {
+func testAccCheckRuleExists(ctx context.Context, ruleName string, accProvider *schema.Provider) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
+		resource := s.RootModule().Resources[ruleName]
 		providerConf := accProvider.Meta().(*ProviderConfiguration)
 		client := providerConf.Client
 
-		if err := ruleExistsHelper(ctx, s, client); err != nil {
+		err := ruleExistsHelper(ctx, resource.Primary.ID, client)
+		if err != nil {
 			return err
 		}
 		return nil
 	}
 }
 
-func ruleExistsHelper(ctx context.Context, s *terraform.State, client *client.JupiterOneClient) error {
-	for _, r := range s.RootModule().Resources {
-		err := resource.RetryContext(ctx, 10*time.Second, func() *resource.RetryError {
-			id := r.Primary.ID
-			ruleInstance, err := client.GetQuestionRuleInstanceByID(id)
+func ruleExistsHelper(ctx context.Context, id string, client *client.JupiterOneClient) error {
+	err := resource.RetryContext(ctx, 10*time.Second, func() *resource.RetryError {
+		ruleInstance, err := client.GetQuestionRuleInstanceByID(id)
 
-			if ruleInstance != nil {
-				return nil
-			}
-
-			if err != nil && strings.Contains(err.Error(), "Rule instance does not exist.") {
-				return resource.RetryableError(fmt.Errorf("Rule instance does not exist (id=%q)", id))
-			}
-
-			return resource.NonRetryableError(err)
-		})
-
-		if err != nil {
-			return err
+		if ruleInstance != nil {
+			return nil
 		}
+
+		if err != nil && strings.Contains(err.Error(), "Rule instance does not exist.") {
+			return resource.RetryableError(fmt.Errorf("Rule instance does not exist (id=%q)", id))
+		}
+
+		return resource.NonRetryableError(err)
+	})
+
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func testAccCheckRuleInstanceDestroy(ctx context.Context, accProvider *schema.Provider) func(*terraform.State) error {
+func testAccCheckRuleInstanceDestroy(ctx context.Context, resourceName string, accProvider *schema.Provider) func(*terraform.State) error {
 	return func(s *terraform.State) error {
+		resource := s.RootModule().Resources[resourceName]
 		providerConf := accProvider.Meta().(*ProviderConfiguration)
 		client := providerConf.Client
 
-		if err := ruleInstanceDestroyHelper(ctx, s, client); err != nil {
+		if err := ruleInstanceDestroyHelper(ctx, resource.Primary.ID, client); err != nil {
 			return err
 		}
 		return nil
 	}
 }
 
-func ruleInstanceDestroyHelper(ctx context.Context, s *terraform.State, client *client.JupiterOneClient) error {
-	for _, r := range s.RootModule().Resources {
-		err := resource.RetryContext(ctx, 30*time.Second, func() *resource.RetryError {
-			id := r.Primary.ID
-			ruleInstance, err := client.GetQuestionRuleInstanceByID(id)
+func ruleInstanceDestroyHelper(ctx context.Context, id string, client *client.JupiterOneClient) error {
+	err := resource.RetryContext(ctx, 30*time.Second, func() *resource.RetryError {
+		ruleInstance, err := client.GetQuestionRuleInstanceByID(id)
 
-			if ruleInstance != nil {
-				return resource.RetryableError(fmt.Errorf("Rule instance still exists (id=%q)", id))
-			}
-
-			if err != nil && strings.Contains(err.Error(), "Rule instance does not exist.") {
-				return nil
-			}
-
-			return resource.NonRetryableError(err)
-		})
-
-		if err != nil {
-			return err
+		if ruleInstance != nil {
+			return resource.RetryableError(fmt.Errorf("Rule instance still exists (id=%q)", id))
 		}
+
+		if err != nil && strings.Contains(err.Error(), "Rule instance does not exist.") {
+			return nil
+		}
+
+		return resource.NonRetryableError(err)
+	})
+
+	if err != nil {
+		return err
 	}
 
 	return nil
