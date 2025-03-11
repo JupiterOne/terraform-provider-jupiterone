ab8bf9fc03d8f6309844dc9d96b7960b6b343347
bjoepfeiffer <brandon.pfeiffer@jupiterone.com>
2025-03-11 19:02:39 -0400

    IDEA-1228: Add account parameters.

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/jupiterone/terraform-provider-jupiterone
 
-go 1.18
+go 1.22.0
+
+toolchain go1.22.3
 
 require (
 	github.com/Khan/genqlient v0.5.0
@@ -9,13 +11,13 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.2
 	github.com/hashicorp/go-hclog v1.5.0
 	github.com/hashicorp/terraform-plugin-docs v0.13.0
-	github.com/hashicorp/terraform-plugin-framework v1.3.2
+	github.com/hashicorp/terraform-plugin-framework v1.14.1
 	github.com/hashicorp/terraform-plugin-framework-validators v0.9.0
-	github.com/hashicorp/terraform-plugin-go v0.17.0
+	github.com/hashicorp/terraform-plugin-go v0.26.0
 	github.com/hashicorp/terraform-plugin-log v0.9.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.27.0
 	github.com/hashicorp/terraform-plugin-testing v1.3.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.8.3
 	gopkg.in/dnaeon/go-vcr.v3 v3.1.2
 )
 
@@ -49,7 +51,7 @@ require (
 	github.com/breml/bidichk v0.2.3 // indirect
 	github.com/breml/errchkjson v0.3.0 // indirect
 	github.com/butuzov/ireturn v0.1.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/charithe/durationcheck v0.0.9 // indirect
 	github.com/chavacava/garif v0.0.0-20220316182200-5cad0b5181d4 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
@@ -74,7 +76,7 @@ require (
 	github.com/go-xmlfmt/xmlfmt v0.0.0-20191208150333-d5b6f63a941b // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gofrs/flock v0.8.1 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golangci/check v0.0.0-20180506172741-cfe4005ccda2 // indirect
 	github.com/golangci/dupl v0.0.0-20180902072040-3e9179ac440a // indirect
 	github.com/golangci/go-misc v0.0.0-20220329215616-d24fe342adfe // indirect
@@ -84,8 +86,8 @@ require (
 	github.com/golangci/misspell v0.3.5 // indirect
 	github.com/golangci/revgrep v0.0.0-20210930125155-c22e5001d4f2 // indirect
 	github.com/golangci/unconvert v0.0.0-20180507085042-28b1c447d1f4 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gordonklaus/ineffassign v0.0.0-20210914165742-4cc7213b9bc8 // indirect
 	github.com/gostaticanalysis/analysisutil v0.7.1 // indirect
 	github.com/gostaticanalysis/comment v1.4.2 // indirect
@@ -95,7 +97,7 @@ require (
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-plugin v1.4.10 // indirect
+	github.com/hashicorp/go-plugin v1.6.2 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/hashicorp/hc-install v0.5.2 // indirect
@@ -104,7 +106,7 @@ require (
 	github.com/hashicorp/logutils v1.0.0 // indirect
 	github.com/hashicorp/terraform-exec v0.18.1 // indirect
 	github.com/hashicorp/terraform-json v0.17.0 // indirect
-	github.com/hashicorp/terraform-registry-address v0.2.1 // indirect
+	github.com/hashicorp/terraform-registry-address v0.2.4 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/hexops/gotextdiff v1.0.3 // indirect
@@ -195,24 +197,24 @@ require (
 	github.com/uudashr/gocognit v1.0.5 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.1 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
-	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/yagipy/maintidx v1.0.0 // indirect
 	github.com/yeya24/promlinter v0.2.0 // indirect
 	github.com/zclconf/go-cty v1.13.2 // indirect
 	gitlab.com/bosi/decorder v0.2.1 // indirect
-	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/exp/typeparams v0.0.0-20220218215828-6cf2b201936e // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/net v0.11.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
-	google.golang.org/grpc v1.56.1 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/grpc v1.69.4 // indirect
+	google.golang.org/protobuf v1.36.3 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ jupiterone/resource_account_parameter.go
@@ -0,0 +1,290 @@
+package jupiterone
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/jupiterone/terraform-provider-jupiterone/jupiterone/internal/client"
+)
+
+type AccountParameterResource struct {
+	version string
+	qlient  graphql.Client
+}
+
+// AccountParameterModel is the terraform HCL representation of an account parameter.
+type AccountParameterModel struct {
+	Id     			types.String `json:"id,omitempty" tfsdk:"id"`
+	Name   			types.String `json:"name,omitempty" tfsdk:"name"`
+	Value  			types.String `json:"value,omitempty" tfsdk:"value"`
+	ValueType  	types.String `json:"valueType,omitempty" tfsdk:"value_type"`
+	Secret 			types.Bool   `json:"secret,omitempty" tfsdk:"secret"`
+}
+
+func NewAccountParameterResource() resource.Resource {
+	return &AccountParameterResource{}
+}
+
+// Metadata implements resource.Resource
+func (*AccountParameterResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_account_parameter"
+}
+
+// Schema implements resource.Resource
+func (*AccountParameterResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "A saved JupiterOne Account Parameter.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"name": schema.StringAttribute{
+				Required:    true,
+				Description: "The name of the account parameter. Must be unique",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"value": schema.StringAttribute{
+				Required:    true,
+				Description: "The value of the account parameter. This string value gets parsed based on the value_type.",
+			},
+			"value_type": schema.StringAttribute{
+				Required:    true,
+				Description: "The type of the value.",
+				Validators: []validator.String{
+					stringvalidator.OneOf("string", "number", "boolean"),
+				},
+			},
+			"secret": schema.BoolAttribute{
+				Description: "Wether or not the value is secret. Defaults to false. If it is secret then it cannot be retrieved through the API and will show as changed for every terraform plan.",
+				Optional:    true,
+				Computed:    true,
+				Default:     booldefault.StaticBool(false),
+			},
+		},
+	}
+}
+
+// Configure implements resource.ResourceWithConfigure
+func (r *AccountParameterResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	p, ok := req.ProviderData.(*JupiterOneProvider)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected JupiterOneProvider, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.version = p.version
+	r.qlient = p.Qlient
+}
+
+// Create implements resource.Resource
+func (r *AccountParameterResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data AccountParameterModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var parsedValue, parseError = parseValue(data.ValueType.ValueString(), data.Value.ValueString())
+
+	if parseError != nil {
+		resp.Diagnostics.AddError("failed to parse account parameter value", parseError.Error())
+		return
+	}
+
+	created, err := client.SetAccountParameter(
+		ctx,
+		r.qlient,
+		data.Name.ValueString(),
+		parsedValue,
+		data.Secret.ValueBool(),
+	)
+
+	if err != nil || !created.SetParameter.Success {
+		resp.Diagnostics.AddError("failed to create account parameter", err.Error())
+		return
+	}
+
+	data.Id = types.StringValue(data.Name.ValueString())
+
+	tflog.Trace(ctx, "Created account parameter",
+		map[string]interface{}{"title": data.Name, "id": data.Id})
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Delete implements resource.Resource
+func (r *AccountParameterResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data AccountParameterModel
+
+	// Read Terraform ste into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if _, err := client.DeleteAccountParameter(ctx, r.qlient, data.Id.ValueString()); err != nil {
+		resp.Diagnostics.AddError("failed to delete account parameter", err.Error())
+	}
+}
+
+// Read implements resource.Resource
+func (r *AccountParameterResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data AccountParameterModel
+
+	// Read Terraform state into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	parameterResp, err := client.GetAccountParameter(ctx, r.qlient, data.Name.ValueString())
+	log.Println("Read account parameter:", parameterResp.GetParameter().Name == "")
+
+	if err != nil {
+		if strings.Contains(err.Error(), "does not exist")  {
+			resp.State.RemoveResource(ctx)
+		} else {
+			resp.Diagnostics.AddError("failed to get account parameter", err.Error())
+		}
+		return
+	} else if parameterResp.Parameter.Name == "" {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	data.Name = types.StringValue(parameterResp.Parameter.Name)
+	data.Value = types.StringValue(parseValueAsString(parameterResp.Parameter.Value))
+	data.ValueType = types.StringValue(determineValueType(parameterResp.Parameter.Value))
+	data.Secret = types.BoolValue(parameterResp.Parameter.Secret)
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// ImportState implements resource.ResourceWithImportState
+func (*AccountParameterResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+// Update implements resource.Resource
+func (r *AccountParameterResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data AccountParameterModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var parsedValue, parseError = parseValue(data.ValueType.ValueString(), data.Value.ValueString())
+
+	if parseError != nil {
+		resp.Diagnostics.AddError("failed to parse account parameter value", parseError.Error())
+		return
+	}
+
+	_, err := client.SetAccountParameter(
+		ctx,
+		r.qlient,
+		data.Name.ValueString(),
+		parsedValue,
+		data.Secret.ValueBool(),
+	)
+
+	if err != nil {
+		resp.Diagnostics.AddError("failed to update account parameter", err.Error())
+		return
+	}
+
+	tflog.Trace(ctx, "Updated account parameter",
+		map[string]interface{}{"title": data.Name, "id": data.Id})
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Parse the value based on the value type like in the update and create functions above
+func parseValue(valueType string, value string) (interface{}, error) {
+	if valueType == "boolean" {
+		boolValue, err := strconv.ParseBool(value)
+		if err != nil {
+			return nil, err
+		}
+		return boolValue, nil
+	}
+
+	if (valueType == "number") {
+		numValue, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return nil, err
+		}
+		return numValue, nil
+	}
+
+	return value, nil
+}
+
+// Parse a dynamic interface{} value back to a string
+// value type should not be a param for the function, the function should determine what type of a value interface{} is
+func parseValueAsString(value interface{}) string {
+	switch v := value.(type) {
+	case bool:
+		return strconv.FormatBool(v)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case int16, int32, int64:
+		return fmt.Sprintf("%d", v)
+	case string:
+		return v
+	default:
+		return ""
+	}
+}
+
+// Now I want to return "boolean", "number", or "string" based on the given value interface{}
+func determineValueType(value interface{}) string {
+	switch value.(type) {
+	case bool:
+		return "boolean"
+	case float64, int, int8, int16, int32, int64:
+		return "number"
+	case string:
+		return "string"
+	default:
+		return "string" // default to string if type is unknown
+	}
+}
+
